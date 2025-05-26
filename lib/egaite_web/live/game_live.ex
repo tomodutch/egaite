739e544bba085e833e07c01d6335430d6f789272
tomodutch <4613944+tomodutch@users.noreply.github.com>
2025-05-26 13:47:42 +0900

    Removed unused functions in game live

--- lib/egaite_web/live/game_live.ex
+++ lib/egaite_web/live/game_live.ex
@@ -5,62 +5,21 @@ defmodule EgaiteWeb.GameLive do
   import EgaiteWeb.{CanvasComponent, PlayersListComponent, ChatBoxComponent, RulesComponent}
 
   def mount(%{"id" => game_id}, _session, socket) do
-    try do
-      if connected?(socket) do
-        Phoenix.PubSub.subscribe(Egaite.PubSub, "game:#{game_id}")
-        Phoenix.PubSub.subscribe(Egaite.PubSub, "chat:#{game_id}")
-        Phoenix.PubSub.subscribe(Egaite.PubSub, "game_presence:#{game_id}")
-      end
+    maybe_subscribe(socket, game_id)
 
-      {:ok, players} = Game.get_players(game_id)
-      current_artist = Game.get_current_artist(game_id)
-
-      Game.add_player(game_id, %Player{
-        id: socket.assigns.me.id,
-        name: socket.assigns.me.name
-      })
-
-      socket =
-        socket
-        |> assign(:game_id, game_id)
-        |> assign(:game_started, false)
-        |> assign(:players, Map.values(players))
-        |> assign(:current_artist, current_artist)
-        |> assign(:full_screen, true)
-        |> stream(:messages, [])
-
-      {:ok, socket}
-    catch
-      :exit, {:noproc, _} ->
-        {:ok, _pid} =
-          GameSupervisor.start_game(game_id, %Player{
-            id: socket.assigns.me.id,
-            name: socket.assigns.me.name
-          })
-
-        {:ok, players} = Game.get_players(game_id)
-        current_artist = Game.get_current_artist(game_id)
-
-        Game.add_player(game_id, %Player{
-          id: socket.assigns.me.id,
-          name: socket.assigns.me.name
-        })
-
-        socket =
-          socket
-          |> assign(:game_id, game_id)
-          |> assign(:game_started, false)
-          |> assign(:players, Map.values(players))
-          |> assign(:current_artist, current_artist)
-          |> assign(:full_screen, true)
-          |> stream(:messages, [])
-
-        {:ok, socket}
+    case maybe_start_game(game_id, socket.assigns.me) do
+      {:ok, _pid} -> :ok
+      :already_started -> :ok
     end
+
+    {:ok, initialize_socket(socket, game_id, socket.assigns.me)}
   end
 
   def terminate(_, socket) do
-    Logger.info("Terminating GameLive for game #{socket.assigns.game_id} and player #{socket.assigns.me.id}")
+    Logger.info(
+      "Terminating GameLive for game #{socket.assigns.game_id} and player #{socket.assigns.me.id}"
+    )
+
     if Map.has_key?(socket.assigns, :game_id) do
       Game.remove_player(socket.assigns.game_id, socket.assigns.me.id)
     end
@@ -68,18 +27,21 @@ defmodule EgaiteWeb.GameLive do
     :ok
   end
 
+  def handle_info({:new_message, msg}, socket) do
+    {:noreply, stream_insert(socket, :messages, msg)}
+  end
+
   def handle_info(%{"event" => "game_started", "artist" => artist}, socket) do
     socket =
       socket
       |> assign(:game_started, true)
       |> assign(:current_artist, artist)
-      |> stream_insert(:messages, %{
-        id: System.unique_integer([:positive]),
-        body: "The game has started! Get ready to draw and guess!",
-        name: "System"
-      })
+      |> stream_insert(
+        :messages,
+        system_msg("The game has started! Get ready to draw and guess!")
+      )
 
-    {:noreply, assign(socket, game_started: true)}
+    {:noreply, socket}
   end
 
   def handle_info(
@@ -91,39 +53,33 @@ defmodule EgaiteWeb.GameLive do
         },
         socket
       ) do
-    msg = %{
-      id: System.unique_integer([:positive]),
-      body: "Starting round #{current_round} of #{max_rounds}. The artist is now #{artist}.",
-      name: "System"
-    }
+    message =
+      system_msg("Starting round #{current_round} of #{max_rounds}. The artist is now #{artist}.")
 
     {:noreply,
      socket
-     |> assign(game_started: true)
-     |> assign(current_artist: artist)
-     |> stream_insert(:messages, msg)}
+     |> assign(:game_started, true)
+     |> assign(:current_artist, artist)
+     |> stream_insert(:messages, message)}
   end
 
   def handle_info(%{event: "presence_diff"}, socket) do
-    game_id = socket.assigns.game_id
-
-    # Re-fetch the latest list of players
-    {:ok, players} = Game.get_players(game_id)
-
+    {:ok, players} = Game.get_players(socket.assigns.game_id)
     {:noreply, assign(socket, players: Map.values(players))}
   end
 
   def handle_event("start", _params, socket) do
-    game_id = socket.assigns.game_id
-    {:ok, _word_to_guess} = Game.start(game_id)
+    Game.start(socket.assigns.game_id)
     {:noreply, socket}
   end
 
   def handle_event("send_message", %{"body" => body}, socket) do
-    if String.trim(body) != "" do
+    trimmed = String.trim(body)
+
+    if trimmed != "" do
       msg = %{
         id: System.unique_integer([:positive]),
-        body: body,
+        body: trimmed,
         name: socket.assigns.me.name
       }
 
@@ -133,48 +89,68 @@ defmodule EgaiteWeb.GameLive do
         {:new_message, msg}
       )
 
-      case Game.guess(socket.assigns.game_id, socket.assigns.me.id, body) do
-        {:ok, :hit} ->
-          {:noreply,
-           socket
-           |> stream_insert(:messages, %{
-             id: System.unique_integer([:positive]),
-             body: "#{socket.assigns.me.name} guessed the word!",
-             name: "System"
-           })
-           |> stream_insert(:messages, msg)}
+      reply_socket =
+        case Game.guess(socket.assigns.game_id, socket.assigns.me.id, trimmed) do
+          {:ok, :hit} ->
+            socket
+            |> stream_insert(:messages, system_msg("#{msg.name} guessed the word!"))
+            |> stream_insert(:messages, msg)
 
-        {:ok, :miss} ->
-          {:noreply, stream_insert(socket, :messages, msg)}
+          _ ->
+            stream_insert(socket, :messages, msg)
+        end
 
-        {:error, :artist_can_not_guess} ->
-          {:noreply, stream_insert(socket, :messages, msg)}
-
-        {:error, :word_not_set} ->
-          {:noreply, stream_insert(socket, :messages, msg)}
-      end
+      {:noreply, reply_socket}
     else
       {:noreply, socket}
     end
   end
 
-  def handle_info({:new_message, msg}, socket) do
-    {:noreply, stream_insert(socket, :messages, msg)}
+  defp maybe_subscribe(socket, game_id) do
+    if connected?(socket) do
+      Phoenix.PubSub.subscribe(Egaite.PubSub, "game:#{game_id}")
+      Phoenix.PubSub.subscribe(Egaite.PubSub, "chat:#{game_id}")
+      Phoenix.PubSub.subscribe(Egaite.PubSub, "game_presence:#{game_id}")
+    end
+  end
+
+  defp maybe_start_game(game_id, player) do
+    try do
+      Game.add_player(game_id, %Player{id: player.id, name: player.name})
+      :already_started
+    catch
+      :exit, {:noproc, _} ->
+        {:ok, pid} = GameSupervisor.start_game(game_id, %Player{id: player.id, name: player.name})
+        {:ok, pid}
+    end
+  end
+
+  defp initialize_socket(socket, game_id, player) do
+    {:ok, players} = Game.get_players(game_id)
+    current_artist = Game.get_current_artist(game_id)
+
+    socket
+    |> assign(:game_id, game_id)
+    |> assign(:game_started, false)
+    |> assign(:players, Map.values(players))
+    |> assign(:current_artist, current_artist)
+    |> assign(:full_screen, true)
+    |> stream(:messages, [])
+  end
+
+  defp system_msg(body) do
+    %{id: System.unique_integer([:positive]), body: body, name: "System"}
   end
 
   defp waiting_room(assigns) do
     ~H"""
     <div class="relative flex flex-col h-full justify-between">
       <%= if @current_artist == @me.id do %>
-        <!-- Overlay for artist with rules and start button -->
         <div
           class="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-70 text-white z-10 p-6"
           style="pointer-events:auto;"
         >
-          <div class="max-w-lg w-full mb-6">
-            <.rules />
-          </div>
-
+          <div class="max-w-lg w-full mb-6"><.rules /></div>
           <button
             phx-click="start"
             type="button"
@@ -185,9 +161,7 @@ defmodule EgaiteWeb.GameLive do
         </div>
       <% else %>
         <div class="absolute inset-0 flex flex-col items-center justify-center bg-black bg-opacity-30 text-white text-center z-10 p-6 pointer-events-none">
-          <div class="max-w-lg w-full mb-4">
-            <.rules />
-          </div>
+          <div class="max-w-lg w-full mb-4"><.rules /></div>
           <p class="italic text-lg">Waiting for the artist to start the game...</p>
         </div>
       <% end %>
@@ -201,7 +175,7 @@ defmodule EgaiteWeb.GameLive do
     """
   end
 
-  @spec render(any) :: Phoenix.LiveView.Rendered.t()
+  @spec render(map()) :: Phoenix.LiveView.Rendered.t()
   def render(assigns) do
     ~H"""
     <div
@@ -223,12 +197,9 @@ defmodule EgaiteWeb.GameLive do
 
     <!-- Right: Players and Chat -->
       <aside class="w-full md:w-[40%] h-1/2 md:h-full flex flex-col">
-        <!-- Players -->
         <div class="h-1/2 overflow-auto border-b border-gray-300">
           <.players_list players={@players} artist={@current_artist} />
         </div>
-
-    <!-- Chat -->
         <div class="h-1/2 overflow-auto">
           <.chat_box messages={@streams.messages} />
         </div>
