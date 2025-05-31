f0a8457de139bfaf94cfaaba27a9d6e75180d8f3
tomodutch <4613944+tomodutch@users.noreply.github.com>
2025-05-31 18:36:11 +0900

    Seed db on deploy

--- lib/egaite/release.ex
+++ lib/egaite/release.ex
@@ -13,6 +13,22 @@ defmodule Egaite.Release do
     end
   end
 
+  def seed do
+    load_app()
+
+    for repo <- repos() do
+      seed_file = seed_path(repo)
+      IO.puts("🔍 Checking for seed file at #{seed_file}")
+
+      if File.exists?(seed_file) do
+        IO.puts("🌱 Running seeds for #{inspect(repo)}")
+        Code.eval_file(seed_file)
+      else
+        IO.puts("⚠️  No seed file found for #{inspect(repo)}")
+      end
+    end
+  end
+
   def rollback(repo, version) do
     load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
@@ -24,5 +40,15 @@ defmodule Egaite.Release do
 
   defp load_app do
     Application.load(@app)
+  end
+
+  defp seed_path(repo), do: Path.join(priv_path(repo), "seeds.exs")
+
+  defp priv_path(repo) do
+    repo
+    |> Module.split()
+    |> List.first()
+    |> Macro.underscore()
+    |> (fn path -> Application.app_dir(@app, "priv/#{path}/repo") end).()
   end
 end
